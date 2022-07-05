7ee92b8c99433e0676931ea7f7cc2a8615311074
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-05 17:13:19 +0200

    Add header component with his style and route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import TopBar from '../components/TopBar'
+import TopBar from '../components/TopBar';
+import Header from '../components/Header';
+
+import "../pages/home.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className='Home'>
       <TopBar />
+      <Header />
     </div>
   )
 }
