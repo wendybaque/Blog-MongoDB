974e5d4a61bfb216267da929f7a4c7fa7ba32bc9
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-07 16:16:04 +0200

    Add post route

--- api/index.js
+++ api/index.js
@@ -2,18 +2,26 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-}).then(console.log("CONNECTEEEEEEEEEEED TO MONGODB")).catch((err) => console.log(err));
+    useFindAndModify:true
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
-app.use("/", (req, res ) => {
-    console.log("Hey this is main url")
-})
+  app.use("/api/auth", authRoute);
+  
+// app.use("/", (req, res ) => {
+//     console.log("Hey this is main url")
+// })
 
 app.listen("5000", () => {
     console.log("Backend is running");
