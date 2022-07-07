3c6deb777955be769f980a889723aa13ed716fe3
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-07 15:18:39 +0200

    Add parameters to connect at mongoose db

--- api/index.js
+++ api/index.js
@@ -5,6 +5,12 @@ const mongoose = require("mongoose");
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(console.log("CONNECTEEEEEEEEEEED TO MONGODB")).catch((err) => console.log(err));
+
 app.use("/", (req, res ) => {
     console.log("Hey this is main url")
 })
