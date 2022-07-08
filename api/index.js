de80b0461655936b410bf06924fe6f0a7897daad
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-08 15:31:20 +0200

    Restore index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users.js");
+const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
-
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -21,36 +21,27 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-  // Stockage des fichiers avec Multer :
-  const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "images");
-    },
-    filename: (req, file, cb) => {
-      cb(null, req.body.name);
-    },
-  });
+  // stockage d'un fichier avec multer :
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-  // Upload des fichiers avec multer :
-  const upload = multer({ storage: storage });
-    app.post("/api/upload", upload.single("file"), (req, res) => {
-    res.status(200).json("Ton image a bien été sauvegardée.");
-  });
+// Upload des fichiers avec multer
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
-  // Route d'autentification :
-  app.use("/api/auth", authRoute);
-  // Route pour les utilisateurs :
-  app.use("/api/users", userRoute);
-  // Route pour les posts : 
-  app.use("/api/posts", postRoute);
-  // Route pour les catégories :
-  app.use("/api/categories", categoryRoute);
-
-  // URL de base :
-// app.use("/", (req, res ) => {
-//     console.log("Hey this is main url")
-// })
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
-    console.log("Backend is running");
+  console.log("Backend is running.");
 });
