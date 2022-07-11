1d7e4aa5f847f60e58113efe6014581d4f43225b
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-11 18:34:45 +0200

    Add useEffet and useState in register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import {Helmet} from "react-helmet";
-
-import '../pages/register.css';
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import "../pages/register.css";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const res = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+    } catch (err) {
+      setError(true);
+    }
+  };
   return (
     <div className='Register'>
          <Helmet>
@@ -13,16 +32,17 @@ export default function Register() {
                 <link rel="canonical" href="http://mysite.com/example" />
         </Helmet>
         <span className="RegisterTitle">CREER UN COMPTE</span>
-      <form className="RegisterForm">
+      <form className="RegisterForm" onSubmit={handleSubmit}>
         <label>Identifiant</label>
-        <input className="RegisterInput" type="email" placeholder="Saisis ton nom d'utilisateur"></input>
+        <input className="RegisterInput" type="text" placeholder="Saisis ton nom d'utilisateur" onChange={(e) => setUsername(e.target.value)}></input>
         <label>E-mail</label>
-        <input className="RegisterInput" type="email" placeholder="Saisis ton email (ex : michel@gmail.com)"></input>
+        <input className="RegisterInput" type="email" placeholder="Saisis ton email (ex : michel@gmail.com)"  onChange={(e) => setEmail(e.target.value)}></input>
         <label>Mot de passe</label>
-        <input className="RegisterInput" type="password" placeholder="Saisis ton mot de passe"></input>
-        <button className="RegisterButton">S'enregistrer</button>
+        <input className="RegisterInput" type="password" placeholder="Saisis ton mot de passe" onChange={(e) => setPassword(e.target.value)}></input>
+        <button className="RegisterButton" type="submit">S'enregistrer</button>
       </form>
       <Link to="/Login"><button className="RegisterLoginButton">Se connecter</button></Link>
+      {error && <span style={{color:"tomato", marginTop:"10px"}}>Quelque chose n'est pas bon. 😉</span>}
     </div>
   )
 }
