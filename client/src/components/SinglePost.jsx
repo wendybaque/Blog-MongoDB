44428dfb24ee0c467e51bd682db0934dae90b55d
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-16 14:01:38 +0200

    Add delete and modify a post

--- client/src/components/SinglePost.jsx
+++ client/src/components/SinglePost.jsx
@@ -1,48 +1,111 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "../components/singlepost.css";
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from "../context/Context";
+
 import axios from 'axios';
 
 export default function SinglePost() {
     const location = useLocation();
     const path = location.pathname.split("/")[2];
     const [post, setPost] = useState({});
-    const PF = "localhost:5000/image/"
+    const PF = "localhost:5000/image/";
+    const {user} = useContext(Context);
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [updateMode, setUpdateMode] = useState(false);
 
+
+    const handleDelete = async () => {
+      try {
+        await axios.delete(`/posts/${post._id}`, {
+          data: { username: user.username },
+        });
+        window.location.replace("/");
+      } catch (err) {}
+    };
+
+    const handleUpdate = async () => {
+      try {
+        await axios.put(`/posts/${post._id}`, {
+          username: user.username,
+          title,
+          desc,
+        });
+        setUpdateMode(false)
+      } catch (err) {}
+    };
 
     useEffect(() => {
         const getPost = async() => {
             const res = await axios.get("/posts/" + path);
-            setPost(res.data)
+            setPost(res.data);
+            setTitle(res.data.title);
+            setDesc(res.data.desc);
         };
         getPost()
     }, [path]);
 
   return (
-    <div className='SinglePost'>
-        <div className="SinglePostWrapper">
-            {post.photo && (
-            <img src={PF + post.photo} alt="Livre au bord de la mer" className="SinglePostImg" /> )}
-            <h1 className="SinglePostTitle">{post.title}
-                <div className="SinglePostEdit">
-                    <i className="SinglePostIcon fa-solid fa-pen-to-square"></i>
-                    <i className="SinglePostIcon fa-solid fa-trash-can"></i>
-                </div>
-            </h1>
-            <div className="SinglePostInfo">
-            <span className="singlePostAuthor">
+    <div className="SinglePost">
+      <div className="SinglePostWrapper">
+        {post.photo && (
+          <img src={PF + post.photo} alt="" className="SinglePostImg" />
+        )}
+        {updateMode ? (
+          <input
+            type="text"
+            value={title}
+            className="SinglePostTitleInput"
+            autoFocus
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        ) : (
+          <h1 className="SinglePostTitle">
+            {title}
+            {post.username === user?.username && (
+              <div className="SinglePostEdit">
+                <i
+                  className="SinglePostIcon far fa-edit"
+                  onClick={() => setUpdateMode(true)}
+                ></i>
+                <i
+                  className="SinglePostIcon far fa-trash-alt"
+                  onClick={handleDelete}
+                ></i>
+              </div>
+            )}
+          </h1>
+        )}
+        <div className="SinglePostInfo">
+          <span className="SinglePostAuthor">
             Author:
             <Link to={`/?user=${post.username}`} className="link">
               <b> {post.username}</b>
             </Link>
           </span>
-                <span className='SinglePostDate'> {new Date(post.createdAt).toDateString()}</span>
-            </div>
-            <p className='SinglePostDescr'>{post.desc}</p>
+          <span className="SinglePostDate">
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
+        {updateMode ? (
+          <textarea
+            className="SinglePostDescInput"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        ) : (
+          <p className="SinglePostDesc">{desc}</p>
+        )}
+        {updateMode && (
+          <button className="SinglePostButton" onClick={handleUpdate}>
+            Modifier
+          </button>
+        )}
+      </div>
     </div>
-  )
+  );
 }
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -10,54 +10,71 @@ export default function Write() {
     const [title, setTitle] = useState("");
     const [desc, setDesc] = useState("");
     const [file, setFile] = useState(null);
-    const {user} = useContext(Context);
-
+    const { user } = useContext(Context);
+  
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        const newPost = {
-            username:user.username,
-            title,
-            desc,
-        };
-        if(file){
-            const data = new FormData();
-            const filename = Date.now() + file.name;
-            data.append("name",filename);
-            data.append("file",file);
-            newPost.photo = filename;
-            try {
-                await axios.post("/upload", data);
-            } catch(err) {}
-        }
+      e.preventDefault();
+      const newPost = {
+        username: user.username,
+        title,
+        desc,
+      };
+      if (file) {
+        const data =new FormData();
+        const filename = Date.now() + file.name;
+        data.append("name", filename);
+        data.append("file", file);
+        newPost.photo = filename;
         try {
-            const res = await axios.post("/post", newPost);
-            window.location.replace("post" + res.data._id)
-        } catch(err) {}
-       
+          await axios.post("/upload", data);
+        } catch (err) {}
+      }
+      try {
+        const res = await axios.post("/posts", newPost);
+        window.location.replace("/post/" + res.data._id);
+      } catch (err) {}
     };
-
-  return (
-    <div className='Write'>
+    return (
+      <div className="Write">
         <Helmet>
                 <meta charSet="utf-8" />
                 <title>Ecrire une chronique | Blog d'Antiigone</title>
                 <link rel="canonical" href="http://mysite.com/example" />
-        </Helmet>
+      </Helmet>
         {file && (
-        <img className="WriteImg" src={URL.createObjectURL(file)} alt="Rangée de livres anciens sur des marches de pierre" />)}
-        <form className='WriteForm' onSubmit={handleSubmit}>
-            <div className="WriteFormGroup">
-                <label htmlFor='fileinput'><i className="WriteIcon fa-solid fa-plus"></i></label>
-                <input type="file" id="fileinput" style={{display:"none"}} onChange={(e) => setFile(e.target.files[0])}></input>
-                <input type="text" id="title" placeholder='Titre' className='WriteInput' autoFocus={true}  onChange={(e) => setTitle(e.target.value)}></input>
-            </div>
-            <div className="WriteFormGroup">
-                <textarea type="text" placeholder='Ecris ta chronique' className='WriteInput WriteText' onChange={(e) => setDesc(e.target.value)}></textarea>
-            </div>
-            <button type="submit" className="WriteSubmit">
-                Publier
-            </button>
+          <img className="WriteImg" src={URL.createObjectURL(file)} alt="" />
+        )}
+        <form className="WriteForm" onSubmit={handleSubmit}>
+          <div className="WriteFormGroup">
+            <label htmlFor="fileInput">
+              <i className="WriteIcon fas fa-plus"></i>
+            </label>
+            <input
+              type="file"
+              id="fileInput"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <input
+              type="text"
+              placeholder="Title"
+              className="WriteInput"
+              autoFocus={true}
+              onChange={e=>setTitle(e.target.value)}
+            />
+          </div>
+          <div className="WriteFormGroup">
+            <textarea
+              placeholder="Tell your story..."
+              type="text"
+              className="WriteInput WriteText"
+              onChange={e=>setDesc(e.target.value)}
+            ></textarea>
+          </div>
+          <button className="WriteSubmit" type="submit">
+            Publish
+          </button>
         </form>
-    </div>
-  );
-}
+      </div>
+    );
+  }
